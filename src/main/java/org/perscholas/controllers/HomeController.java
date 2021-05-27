aec5575176003bf827350f6bac92ef4a77cdf425
Hunter White <HWB94@protonmail.com>
2021-05-27 14:18:48 -0400

    Changes by David

--- src/main/java/org/perscholas/controllers/HomeController.java
+++ src/main/java/org/perscholas/controllers/HomeController.java
@@ -40,4 +40,87 @@ public class HomeController {
         return "template";
     }
 
+    @GetMapping("/allStudents")
+    public String allStudents(Model model) {
+        model.addAttribute("allStudents", studentService.getAllStudent());
+        return "allStudents";
+    }
+
+    @GetMapping("/allCourses")
+    public String allCourses(Model model) {
+        model.addAttribute("allCourses", courseService.getAllCourses());
+        return "allCourses";
+    }
+
+    @GetMapping("/student")
+    public String studentHome() {
+        return "studentConfirmation";
+    }
+
+    @GetMapping("/student/register")
+    public String studentRegistration(@SessionAttribute("student") Student student){
+        return "studentRegistration";
+    }
+
+    @PostMapping("/student/register")
+    public String studentRegister(@ModelAttribute("student") @Valid Student student, BindingResult result, Model model) {
+        System.out.println(result.hasErrors());
+        if(result.hasErrors()) {
+            return "studentRegistration";
+
+        }else{
+            Student databaseStudent = studentService.saveStudent(student);
+            model.addAttribute("student", student);
+            return "redirect:/student";
+        }
+    }
+
+    @GetMapping("/course/register")
+    public String courseRegistration() {
+        return "courseRegistration";
+    }
+
+    @PostMapping("/course/register")
+    public String courseRegister(@ModelAttribute("course") @Valid Course course, BindingResult result, Model model) {
+        System.out.println(result.hasErrors());
+        if(result.hasErrors()) {
+            return "courseRegistration";
+
+        }else{
+            System.out.println("Course ID: " + course.getId());
+            Course newCourse = courseService.saveCourse(course);
+            return "courseConfirmation";
+        }
+    }
+
+    @GetMapping("/course/registerStudent")
+    public String courseStudentRegistration(@SessionAttribute("student") Student student, Model model){
+
+        if(student.getEmail() == null) {
+            return "redirect:/student/login";
+        }
+
+        return "courseRegistration";
+    }
+
+
+    @PostMapping("/course/registerStudent")
+    public String courseRegisterStudent(
+            @ModelAttribute("student") @Valid Student student, BindingResult result, Model model) {
+        System.out.println(result.hasErrors());
+        if(result.hasErrors()) {
+            return "courseRegistration";
+
+        }else{
+            //Student databaseStudent = studentServices.saveStudent(student);
+            //student.setCourses(student.getCourses().add(course));
+            return "studentConfirmation";
+        }
+    }
+
+    @GetMapping("getsession")
+    public String getSession(){
+    return "getsession";
+    }
+
 }
